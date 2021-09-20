66157024172d5b0b7b31fb6c5962b219fb2871e7
diwao <diwao@me.com>
2021-09-20 09:09:20 +0900

    refactor: Add user type file

--- app/src/ts/services/userService.ts
+++ app/src/ts/services/userService.ts
@@ -1,30 +1,6 @@
 import { AxiosResponse } from 'axios';
 import { apiClient } from './apiClient';
 
-type User = {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-};
-
 export async function fetchUsers(): Promise<AxiosResponse<User[]>> {
   return apiClient.get('/users');
 }
